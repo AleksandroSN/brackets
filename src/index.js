a7cd4df1da3d4a3af830af694155c5a00844f60d
AleksandroSN <59736420+AleksandroSN@users.noreply.github.com>
2021-02-09 19:19:26 +0300

    feat: solution through stack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-module.exports = function check(str, bracketsConfig) {
-    // if (str.length % 2 != 0) return false;
-
+/*module.exports = function check(str, bracketsConfig) {
     for (let i = str.length; i >= 0; i--) {
         bracketsConfig.forEach((elem) => {
             let compareStr = elem.join("");
@@ -11,4 +9,20 @@ module.exports = function check(str, bracketsConfig) {
     }
 
     return str.length === 0 ? true : false;
-};
+};*/
+
+//решение через стек
+module.exports = function check(str, bracketsConfig) {
+    let stack = [];
+    let bracketsPair = bracketsConfig.flat();
+    for (let bracket of str) {
+        let bracketsIndex = bracketsPair[bracketsPair.indexOf(bracket) + 1];
+        if (bracket === stack[stack.length - 1]) {
+            stack.pop();
+        } else {
+            stack.push(bracketsIndex);
+        }
+    }
+
+    return stack.length === 0 ? true : false;
+}
